db3e19ec94c116b676950e48206467b76a5ecac7
aleksandr <avdonin.twistzz@mail.ru>
2025-01-27 19:45:49 +0300

    sel and playwright

--- test_playwright.py
+++ test_playwright.py
@@ -1,8 +1,9 @@
 import pytest
 from playwright.sync_api import Playwright, sync_playwright, expect
-
+from time import sleep
 def test_add_todo(page):
     page.goto("https://demo.playwright.dev/todomvc/#/")
     page.get_by_placeholder("What needs to be done?").click()
     page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
     page.get_by_placeholder("What needs to be done?").press("Enter")
+    sleep(5)
